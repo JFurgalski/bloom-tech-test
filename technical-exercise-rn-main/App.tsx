c60a18315500989e8f24f9a16cf188aafda10b99
Yaz Furgalski <jaroslaw.furgalski@spotlightsportsgroup.com>
2023-12-10 23:35:09 +0000

    Creating SingleStreak skeleton with its file structure
    I dropped this in app.tsx to make sure its rendering as expected

--- technical-exercise-rn-main/App.tsx
+++ technical-exercise-rn-main/App.tsx
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { projectPalette } from './src/styles/projectPallete';
+import SingleStreak from './src/components/SingleStreak/SingleStreak';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <StatusBar style="light" />
+      <SingleStreak />
     </View>
   );
 }
---
+++ technical-exercise-rn-main/src/components/SingleStreak/SingleStreak.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { View, Text } from "react-native";
+import { styles } from "./SingleStreak.styles";
+
+const SingleStreak: React.FC<SingleStreakProps> = ({}) => {
+  return (
+    <View style={styles.container}>
+      <Text>Test</Text>
+    </View>
+  );
+};
+
+export default SingleStreak;
