f67a2586a7f7dba7260df4d8e9c49c0646dc21b2
Yaz Furgalski <jaroslaw.furgalski@spotlightsportsgroup.com>
2023-12-10 23:22:32 +0000

    Adding a projectPallete for commonStyles
    I can see there are two gradients that will most likely be used as well as some common colours I will reuse in components

---
+++ technical-exercise-rn-main/src/styles/projectPallete.ts
@@ -0,0 +1,13 @@
+const projectPalette = {
+  streakGradient: ["#E32EED", "#F76938"],
+  darkGray: "#3b3b3b",
+  foundationPrimary: "#F7F7F7",
+  black: "#000000",
+  backgroundDark: "#1F1F1F",
+  perfectStreakGradient: [
+    "rgba(247, 105, 56, 0.6)",
+    "rgba(227, 46, 237, 0.00)",
+  ],
+};
+
+export { projectPalette };
