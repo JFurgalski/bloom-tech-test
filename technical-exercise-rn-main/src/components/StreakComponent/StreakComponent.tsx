befaad9d5e2096502b280c985c1bfa7415776e63
Yaz Furgalski <jaroslaw.furgalski@spotlightsportsgroup.com>
2023-12-11 00:02:01 +0000

    Changing the prop so I can see what the streak looks like

--- technical-exercise-rn-main/src/components/StreakComponent/StreakComponent.tsx
+++ technical-exercise-rn-main/src/components/StreakComponent/StreakComponent.tsx
@@ -7,7 +7,7 @@ import SingleStreak from "../SingleStreak/SingleStreak";
 const StreakComponent: React.FC<StreakComponentProps> = ({}) => {
   return (
     <View style={styles.container}>
-      <SingleStreak isCurrentDay={true} isPerfectWeek={false} hasStreak={false} />
+      <SingleStreak isCurrentDay={true} isPerfectWeek={false} hasStreak={true} />
       <Text>I am the streak component</Text>
     </View>
   );
