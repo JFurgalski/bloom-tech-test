a729c25e6cdc570975b320ef7a081845fa85a26b
Yaz Furgalski <jaroslaw.furgalski@spotlightsportsgroup.com>
2023-12-11 01:15:19 +0000

    cleanup

--- technical-exercise-rn-main/App.tsx
+++ technical-exercise-rn-main/App.tsx
@@ -1,19 +1,18 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, View } from "react-native";
 import { projectPalette } from "./src/styles/projectPallete";
-import SingleStreak from "./src/components/SingleStreak/SingleStreak";
 import StreakComponent from "./src/components/StreakComponent/StreakComponent";
 
 export default function App() {
-
   const streakData: string[] = [
+    "2023-12-05T00:00:00+00:00",
+    "2023-12-06T00:00:00+00:00",
     "2023-12-07T00:00:00+00:00",
     "2023-12-08T00:00:00+00:00",
     "2023-12-09T00:00:00+00:00",
     "2023-12-10T00:00:00+00:00",
     "2023-12-11T00:00:00+00:00",
   ];
-
 
   return (
     <View style={styles.container}>
