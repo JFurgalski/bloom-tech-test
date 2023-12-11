9b4cce33a30c770c1f08bf107bff40a2fa017d2f
Yaz Furgalski <jaroslaw.furgalski@spotlightsportsgroup.com>
2023-12-11 00:27:48 +0000

    display the days and start populating StreakComp with mock data

--- technical-exercise-rn-main/src/components/StreakComponent/StreakComponent.styles.ts
+++ technical-exercise-rn-main/src/components/StreakComponent/StreakComponent.styles.ts
@@ -1,7 +1,17 @@
 import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
-  container: {},
+  container: {
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  streak: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    width: "90%",
+  },
 });
 
 export { styles };
--- technical-exercise-rn-main/src/components/StreakComponent/StreakComponent.tsx
+++ technical-exercise-rn-main/src/components/StreakComponent/StreakComponent.tsx
@@ -3,6 +3,7 @@ import { View, Text } from "react-native";
 import { styles } from "./StreakComponent.styles";
 import { StreakComponentProps } from "./StreakComponent.types";
 import SingleStreak from "../SingleStreak/SingleStreak";
+import { daysOfWeek } from "../../constants";
 
 const StreakComponent: React.FC<StreakComponentProps> = ({ streak }) => {
   const isFilleDaysCount = streak.filter(Boolean).length;
@@ -16,10 +17,42 @@ const StreakComponent: React.FC<StreakComponentProps> = ({ streak }) => {
     return 0;
   };
 
+  const renderStreak = () => {
+    const currentStreak = streak.filter(Boolean);
+    const currentStreakLength = currentStreak.length;
+    let renderedDaysCount = currentStreakLength % 7;
+
+    const startingDayIndex: number = getStartingDayIndex();
+    const reshuffledDays: string[] = [
+      ...daysOfWeek.slice(startingDayIndex),
+      ...daysOfWeek.slice(0, startingDayIndex),
+    ];
+
+    return reshuffledDays.map((day: string, index: number) => {
+      const isCurrentDay: boolean =
+        index === (new Date().getDay() + 7 - startingDayIndex) % 7;
+      const hasStreak: boolean =
+        index < renderedDaysCount || (renderedDaysCount === 1 && index === 0);
+      const isPerfectWeek =
+        (currentStreakLength / 7) % 1 === 0 && index < renderedDaysCount;
+
+      return (
+        <View>
+          <Text>{day}</Text>
+          <SingleStreak
+            key={index}
+            hasStreak={hasStreak}
+            isCurrentDay={isCurrentDay}
+            isPerfectWeek={isPerfectWeek}
+          />
+        </View>
+      );
+    });
+  };
+
   return (
     <View style={styles.container}>
-      <View></View>
-      <SingleStreak isCurrentDay={true} isPerfectWeek={true} hasStreak={true} />
+      <View style={styles.streak}>{renderStreak()}</View>
     </View>
   );
 };
